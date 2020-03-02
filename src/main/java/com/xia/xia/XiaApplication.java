b6b8d9fb29df65cf3a1ed558a13495c161ca0c8c
zoe9804 <x900124@163.com>
2020-03-02 20:32:59 +0800

    second commit

---
+++ src/main/java/com/xia/xia/XiaApplication.java
@@ -0,0 +1,13 @@
+package com.xia.xia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xia/xia/XiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xia.xia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
